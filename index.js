f323363706a160b2458387537deb848f4c7d9d9e
LBH <ggamit139@gmail.com>
2017-11-30 16:30:10 +0900

    no message

--- index.js
+++ index.js
@@ -1,57 +1,6 @@
-/*
-
-var slideIndex = 0;
-
-function showSlides() {
-    var i, slides = document.getElementsByClassName("mySlides");
-
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    } 
-    slides[slideIndex - 1].style.display = "block"; 
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-    showSlides();
-
-}
-
-showSlides();
-
-*/
-
-
-
-// code for swap images automatically
-
-var slideIndex = 0;
-
-showSlides();
-
-function showSlides() {
-    var i, slides = document.getElementsByClassName("mySlides");
-
-    while(true){        
-        for (i = 0; (i+1) < slides.length; i++) {
-        slides[i].style.display = "none";
-        slides[i+1].style.display = "block";  
-        setTimeout(showSlides, 2000); // Change image every 2 
-    }
-    slideIndex++;
-    
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-    }
-}
-
-
 
 // code for swap images
+
 
 var slideIndex = 1;
 
