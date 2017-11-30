3b87fb3cd978644720d13859e87a1a7490a1b458
LBH <ggamit139@gmail.com>
2017-11-30 15:20:15 +0900

    add new images

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
+/*
+
+var slideIndex = 0;
+
+function showSlides() {
+    var i, slides = document.getElementsByClassName("mySlides");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    } 
+    slides[slideIndex - 1].style.display = "block"; 
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    showSlides();
+
+}
+
+showSlides();
+
+*/
+
+
+
+// code for swap images automatically
+
+var slideIndex = 0;
+
+showSlides();
+
+function showSlides() {
+    var i, slides = document.getElementsByClassName("mySlides");
+
+    while(true){        
+        for (i = 0; (i+1) < slides.length; i++) {
+        slides[i].style.display = "none";
+        slides[i+1].style.display = "block";  
+        setTimeout(showSlides, 2000); // Change image every 2 
+    }
+    slideIndex++;
+    
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
+}
+
+
+
+// code for swap images
+
 var slideIndex = 1;
 
 function showDivs(n) {
@@ -20,3 +75,4 @@ function showDivs(n) {
 function plusDivs(n) {
     showDivs(slideIndex += n);
 }
+
