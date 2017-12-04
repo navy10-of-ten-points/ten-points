2947dce4fde21c3276cf1eca561745bd4c67d563
LBH <ggamit139@gmail.com>
2017-12-04 16:49:23 +0900

    responsive function added. under 600px, slider button becomes smaller. slider fixed with buttons.

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,3 +25,11 @@ function plusDivs(n) {
     showDivs(slideIndex += n);
 }
 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += "responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
